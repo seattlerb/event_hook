ee198ef99c6faadc5fbceb9af92fe02821afe48a
Ryan Davis <ryand@zenspider.com>
2009-05-11 15:28:31 -0800

    Added an actual test [git-p4: depot-paths = "//src/event_hook/dev/": change = 4919]

--- test/test_event_hook.rb
+++ test/test_event_hook.rb
@@ -1,8 +1,51 @@
 require "test/unit"
 require "event_hook"
 
+class Stupid < EventHook
+  @@log = []
+
+  def self.log
+    @@log
+  end
+
+  def self.process(*a)
+    @@log << a
+  end
+end
+
 class TestEventHook < Test::Unit::TestCase
+  def method_1
+    method_2
+  end
+
+  def method_2
+    method_3
+  end
+
+  def method_3
+  end
+
   def test_sanity
-    flunk "write tests or I will kneecap you"
+    Stupid.start_hook
+    method_1
+    Stupid.stop_hook
+
+    actual = Stupid.log.map { |id, obj, meth, klass|
+      [EventHook::EVENTS[id], klass, meth]
+    }
+
+    expected = [[:return, Stupid,        :start_hook],
+                [:call,   TestEventHook, :method_1],
+                [:call,   TestEventHook, :method_2],
+                [:call,   TestEventHook, :method_3],
+                [:return, TestEventHook, :method_3],
+                [:return, TestEventHook, :method_2],
+                [:return, TestEventHook, :method_1],
+                [:call,   Stupid,        :stop_hook],
+                [:call,   Stupid,        :instance],
+                [:return, Stupid,        :instance],
+                [:ccall,  EventHook,     :remove_event_hook]]
+
+    assert_equal expected, actual
   end
 end
